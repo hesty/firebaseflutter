d3ce38298e5ea95b0025bf9bfac77bbbea6cc975
keremduman30 <berwari30_1907@hotmail.com>
2021-01-22 13:44:21 +0300

    Firebase

---
+++ android/app/src/main/kotlin/com/example/firebase_works_superknowa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebase_works_superknowa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home:Anasayfa(),
+    );
+  }
+}
+
+class Anasayfa extends StatefulWidget {
+  @override
+  _AnasayfaState createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  String a;
+  Future<void> veriOku() async{
+    //tum dokumankar
+    await FirebaseFirestore.instance.collection("notlar").get().then((snapchpt){
+      snapchpt.docs.forEach((doc) {
+       debugPrint(" ${doc.data()}");
+     });
+    });
+
+
+
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+   // veriEkle();
+
+    Firebase.initializeApp().whenComplete(() {
+      print("completed");
+      setState(() {});
+    });
+    veriOku();
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("firebase superknowa"),
+      ),
+      body: Center(child: Text("merhaba"),),
+    );
+  }
+}
